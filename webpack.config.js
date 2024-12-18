a85d2a7c1071d4fd6cdf87e8fa1df67134e054da
lisaheinzman <lisaheinzman@knights.ucf.edu>
2024-12-18 18:40:32 -0500

    webpack added

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const createExpoWebpackConfigAsync = require("@expo/webpack-config");
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+
+  // Add polyfills for Node.js modules
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    buffer: require.resolve("buffer"),
+  };
+
+  return config;
+};
